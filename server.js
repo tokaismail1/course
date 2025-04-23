65fa6e20d92b44e8c712c716a91cac88679faa9e
tokaismail1 <tokaismail1122@gmail.com>
2025-04-23 23:05:29 +0200

    task

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ connectDB();
 app.use('/api/courses', courseRoutes);
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('everything is ok');
+  });
 const PORT =5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
